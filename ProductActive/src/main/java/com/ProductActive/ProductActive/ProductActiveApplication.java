567e6b5c8009d69d09699d146c455520f41405b8
Luis Acuña <luisandresverdugox@gmail.com>
2022-11-25 19:18:38 -0500

    Subiendo primer código de Producto Activo

---
+++ ProductActive/src/main/java/com/ProductActive/ProductActive/ProductActiveApplication.java
@@ -0,0 +1,13 @@
+package com.ProductActive.ProductActive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductActiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductActiveApplication.class, args);
+	}
+
+}
---
+++ ProductActive/src/test/java/com/ProductActive/ProductActive/ProductActiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProductActive.ProductActive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductActiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
